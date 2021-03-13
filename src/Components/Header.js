1cff5054ccb81492996e5324364ec3081b4cb197
SaintRhys <cool.rhys@hotmail.co.uk>
2021-03-13 16:05:18 +0000

    header update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,10 +13,8 @@ function Header(props) {
 
   const HeaderButton = ({ name, link }) => {
     return (
-      <Link to={"/" + link} className="flex justify-between">
-        <div class="cursor-pointer flex w-20 justify-center bg-gray-200 m-2 border-black border-2">
-          {name}
-        </div>
+      <Link to={"/" + link} className="flex justify-between mr-3 ml-3">
+        <div class="cursor-pointer flex justify-center ">{name}</div>
       </Link>
     );
   };
@@ -24,33 +22,28 @@ function Header(props) {
   return (
     // a header component that is always displayed at the top of the page
     // plus a navigation componet
-    <header className="border-b p-3 flex justify-between items-center h-20">
-      {/* <Navigation /> */}
-      <div className="p-3 flex ">
-        <div>
-          {login ? (
-            <HeaderButton name="Account" link="account" />
-          ) : (
-            <HeaderButton name="Login" link="login" />
-          )}
-          <HeaderButton name="About Us" link="products" />
-        </div>
-        <div>
-          {login ? null : <HeaderButton name="Register" link="register" />}
-          <HeaderButton name="Products" link="products" />
+    <header>
+      <div className="border-b p-3 flex justify-center items-center h-20 bg-primary">
+        <img
+          className="flex font-bold text-black text-4xl mr-5 text-right h-20 rounded-md "
+          src={logo_big}
+        />
+        <div class="flex inset-y-5 right-0 flex-col items-center justify-between w-1/4 h-full absolute ">
+          <Basket />
         </div>
       </div>
-      <img
-        className="flex font-bold text-black text-4xl mr-5 text-right h-20 rounded-md"
-        src={logo_big}
-      />
-      <div class="flex inset-y-0 right-0 flex-col items-center justify-between w-1/4 h-full">
+      <div class="flex justify-between border-black border-2">
+        <div className="flex">
+          <HeaderButton name="Home" link="" />
+          <HeaderButton name="Products" link="products" />
+          <HeaderButton name="Galactic Items" link="account" />
+          <HeaderButton name="My Account" link="account" />
+        </div>
         <input
           type="text"
-          class="focus:ring-indigo-500 focus:ring-2 ring-2 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-200 rounded-md"
+          class="focus:ring-indigo-500 focus:ring-2 ring-2 focus:border-indigo-500 block w-1/4 pl-7 pr-12 sm:text-sm border-gray-200 rounded-md m-1"
           placeholder={"Search"} // this doesn't do anything, just for filler
         />
-        <Basket />
       </div>
     </header>
   );
